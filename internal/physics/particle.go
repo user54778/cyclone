833f02e2f7b80a77bd05bd17ef1fb891d13fb36c
Ahron Pollak <ahronrocks51@gmail.com>
2025-01-13 15:15:41 -0500

    feat: implement basic force accumulator

--- internal/physics/particle.go
+++ internal/physics/particle.go
@@ -6,34 +6,37 @@ import (
 	"fmt"
 	"math"
 
-	"github.com/user54778/cyclone/internal/math64"
+	m "github.com/user54778/cyclone/internal/math64"
 )
 
 // Particle is the simplest object that can be simulated
 // in a physics system.
 //
-// It is a point mass object; an object with mass, but no size, that CAN move through space but has NO
+// It is a point mass object-an object with mass, but no size, that CAN move through space but has NO
 // internal degrees of freedom (can't rotate).
 type Particle struct {
 	// Describes linear position of the Particle in the world space.
-	Position math64.Vector3
+	Position m.Vector3
 	// Hold linear velocity of the Particle in the world space.
-	Velocity math64.Vector3
+	Velocity m.Vector3
 	// Hold acceleration of the Particle. This can be used to set
-	// acceleration due to gravity, or any other constant acceleration.
-	Acceleration math64.Vector3
+	// acceleration due to gravity, or any other *constant* acceleration.
+	Acceleration m.Vector3
 	// Damping is our solution to give a rough approximation for drag
 	// to apply to our particle in accordance with Newton's First Law.
 	Damping float64
-	// inverseMass is more useful to hold since it makes integration simpler
+	// Inverse Mass is more useful to hold since it makes integration simpler
 	// and is more useful to have objects with infinite mass (i.e., walls, floors, etc)
 	// than storing mass itself, which could (although shouldn't) have zero mass.
-	// This field is equipped with mass/inverseMass setters and private to help with this.
+	// You can set this field with the Mass and InverseMass setters or set it directly.
 	inverseMass float64
+	// forceAccumulator accumulates every force to be applied at the next
+	// iteration *only*. It is zeroed at each integration step.
+	forceAccumulator m.Vector3
 }
 
 // NewParticleMass creates a Particle object where the MASS itself is passed in as a parameter.
-func NewParticleMass(position, velocity, acceleration math64.Vector3, damping, mass float64) Particle {
+func NewParticleMass(position, velocity, acceleration m.Vector3, damping, mass float64) Particle {
 	p := Particle{
 		Position:     position,
 		Velocity:     velocity,
@@ -46,7 +49,7 @@ func NewParticleMass(position, velocity, acceleration math64.Vector3, damping, m
 }
 
 // NewParticleInverseMass creates a Particle object where the INVERSE MASS is passed in as a parameter.
-func NewParticleInverseMass(position, velocity, acceleration math64.Vector3, damping, inverseMass float64) Particle {
+func NewParticleInverseMass(position, velocity, acceleration m.Vector3, damping, inverseMass float64) Particle {
 	p := Particle{
 		Position:     position,
 		Velocity:     velocity,
@@ -87,25 +90,30 @@ func (p *Particle) Mass() float64 {
 	}
 }
 
-// inverseMass accesses the inverseMass directly.
-func (p *Particle) InverseMass() float64 {
-	return p.inverseMass
-}
-
 // KineticEnergy returns the kinetic energy of a particle, given by the
 // equation: K = 1/2m*mag(v)^2.
 func (p *Particle) KineticEnergy() float64 {
 	return 0.5 * p.Mass() * p.Velocity.Magnitude() * p.Velocity.Magnitude()
 }
 
-// Integrate performs Newton-Euler integration, a linear approximation
-// to the correct integral, to integrate a Particle forward in time by duration amount.
+// AddForce adds force to the particle to be applied at the next iteration.
+func (p *Particle) AddForce(force m.Vector3) {
+	p.forceAccumulator.Add(force) // NOTE: This directly adds to the particle's ForceAccumulator,
+	// see if doing this in a functional style does not eat much memory.
+}
+
+func (p *Particle) ClearForces() {
+	p.forceAccumulator = m.Vector3{}
+}
+
+// Integrate updates the position and velocity of a point mass using equations for constant
+// acceleration.
 func (p *Particle) Integrate(duration float64) error {
-	// TODO: Modify to use a logging error system.
+	// TODO: Modify to use physicslog.
 	switch {
 	case p.inverseMass <= 0.0:
 		return fmt.Errorf("integration is not performed on infinite mass")
-	case duration < 0.0:
+	case duration <= 0.0:
 		return fmt.Errorf("can not perform integration on a negative duration")
 	}
 	// NOTE: That I am using pointer methods for Vector operations; copying will result
@@ -118,7 +126,8 @@ func (p *Particle) Integrate(duration float64) error {
 
 	// Update velocity based on acceleration
 	resultingAcceleration := p.Acceleration
-
+	// Apply the force's accumulated to the resulting acceleration.
+	resultingAcceleration.ScaleAdd(p.forceAccumulator, p.inverseMass)
 	// Update linear velocity from the acceleration.
 	p.Velocity.ScaleAdd(resultingAcceleration, duration)
 
@@ -127,14 +136,18 @@ func (p *Particle) Integrate(duration float64) error {
 	dampingFactor := math.Pow(p.Damping, duration)
 	p.Velocity.Scale(dampingFactor)
 
+	// Clear the accumulated force after applying it to the particle.
+	p.ClearForces()
+
 	return nil
 }
 
+/*
 // Deprecated: Only use Integrate() to perform integration. This should only ever be used
 // to compare differences in velocity of the two functions when time is not incorporated in drag.
 func (p *Particle) IntegrateNoTimeScale(duration float64) error {
 	switch {
-	case p.inverseMass <= 0.0:
+	case p.InverseMass <= 0.0:
 		return fmt.Errorf("integration is not performed on infinite mass")
 	case duration < 0.0:
 		return fmt.Errorf("can not perform integration on a negative duration")
@@ -147,3 +160,4 @@ func (p *Particle) IntegrateNoTimeScale(duration float64) error {
 	p.Velocity.Scale(p.Damping)
 	return nil
 }
+*/
